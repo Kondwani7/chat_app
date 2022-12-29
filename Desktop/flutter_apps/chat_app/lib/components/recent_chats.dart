8cba7289376217d25cc4ca4da5a9bbd60027dbe0
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-30 00:41:02 +0200

    recent chats display sender names

--- Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
+++ Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/models/message_model.dart';
 import 'package:flutter/material.dart';
 
 class RecentChats extends StatelessWidget {
@@ -5,6 +6,20 @@ class RecentChats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(chats[index].sender.name);
+        },
+      ),
+    );
   }
 }
