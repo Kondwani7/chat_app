881d7ea01c8fac2e890dbbbee6833ba3eac28b01
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-29 23:56:52 +0200

    favorite contacts header

--- Desktop/flutter_apps/chat_app/lib/components/favorite_contacts.dart
+++ Desktop/flutter_apps/chat_app/lib/components/favorite_contacts.dart
@@ -4,6 +4,35 @@ class FavoriteContacts extends StatelessWidget {
   const FavoriteContacts({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_horiz_outlined),
+                  iconSize: 30.0,
+                  color: Colors.blueGrey,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
