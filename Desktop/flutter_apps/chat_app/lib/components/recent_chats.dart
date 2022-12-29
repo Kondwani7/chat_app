7e7fc110780c7f3e111dac56199ef9267deaea49
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-30 01:39:25 +0200

    cut both the sender image and text when scrolling

--- Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
+++ Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
@@ -15,91 +15,96 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30.0),
           ),
         ),
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (BuildContext context, int index) {
-            final Message chat = chats[index];
-            return Row(
-              //adds spacing between sender image and sender name & text
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  //styling and code for a single message
-                  children: <Widget>[
-                    //sender image
-                    CircleAvatar(
-                      radius: 35.0,
-                      backgroundImage: AssetImage(chat.sender.imageUrl),
-                    ),
-                    const SizedBox(width: 10.0),
-                    Column(
-                      //aligning to start, how individual messages appear on whatsapp
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          chats[index].sender.name,
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 5.0),
-                        Container(
-                          //media query allows you to handle & adapt to dynamic data
-                          //adjusts texts to 0.45 of width of sender row screen
-                          width: MediaQuery.of(context).size.width * 0.45,
-                          child: Text(
-                            chat.text,
+        //when scrolling cut of the sender text and image at the same time
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
+          child: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Message chat = chats[index];
+              return Row(
+                //adds spacing between sender image and sender name & text
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    //styling and code for a single message
+                    children: <Widget>[
+                      //sender image
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(chat.sender.imageUrl),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Column(
+                        //aligning to start, how individual messages appear on whatsapp
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            chats[index].sender.name,
                             style: const TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            //hide long messages
-                            overflow: TextOverflow.ellipsis,
+                                color: Colors.grey,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      chat.time,
-                      style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 5.0,
-                    ),
-                    //handle unread messages
-                    //if unread, show new message alert, else nothing
-                    chat.unread
-                        ? Container(
-                            width: 40.0,
-                            height: 20.0,
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor,
-                              borderRadius: BorderRadius.circular(30.0),
+                          const SizedBox(height: 5.0),
+                          Container(
+                            //media query allows you to handle & adapt to dynamic data
+                            //adjusts texts to 0.45 of width of sender row screen
+                            width: MediaQuery.of(context).size.width * 0.45,
+                            child: Text(
+                              chat.text,
+                              style: const TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              //hide long messages
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            alignment: Alignment.center,
-                            child: const Text(
-                              'NEW',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        : const Text(''),
-                  ],
-                ),
-              ],
-            );
-          },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        chat.time,
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      //handle unread messages
+                      //if unread, show new message alert, else nothing
+                      chat.unread
+                          ? Container(
+                              width: 40.0,
+                              height: 20.0,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColor,
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                              alignment: Alignment.center,
+                              child: const Text(
+                                'NEW',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          : const Text(''),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
