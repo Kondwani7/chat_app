b0d9b1dc4fe8e87702d06371c657b27ddd86e191
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-30 01:48:43 +0200

    clean up styling recent_chats page

--- Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
+++ Desktop/flutter_apps/chat_app/lib/components/recent_chats.dart
@@ -23,85 +23,97 @@ class RecentChats extends StatelessWidget {
             itemCount: chats.length,
             itemBuilder: (BuildContext context, int index) {
               final Message chat = chats[index];
-              return Row(
-                //adds spacing between sender image and sender name & text
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    //styling and code for a single message
-                    children: <Widget>[
-                      //sender image
-                      CircleAvatar(
-                        radius: 35.0,
-                        backgroundImage: AssetImage(chat.sender.imageUrl),
-                      ),
-                      const SizedBox(width: 10.0),
-                      Column(
-                        //aligning to start, how individual messages appear on whatsapp
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            chats[index].sender.name,
-                            style: const TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 5.0),
-                          Container(
-                            //media query allows you to handle & adapt to dynamic data
-                            //adjusts texts to 0.45 of width of sender row screen
-                            width: MediaQuery.of(context).size.width * 0.45,
-                            child: Text(
-                              chat.text,
+              return Container(
+                //add some spacing between each sender message & image
+                margin: const EdgeInsets.only(top: 5.0, right: 20.0),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 10.0),
+                //if unread, sender message secondary color, else(read) white
+                decoration: BoxDecoration(
+                    color: chat.unread ? const Color(0xFFFFEFEE) : Colors.white,
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0))),
+                child: Row(
+                  //adds spacing between sender image and sender name & text
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      //styling and code for a single message
+                      children: <Widget>[
+                        //sender image
+                        CircleAvatar(
+                          radius: 35.0,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Column(
+                          //aligning to start, how individual messages appear on whatsapp
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              chats[index].sender.name,
                               style: const TextStyle(
-                                color: Colors.blueGrey,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              //hide long messages
-                              overflow: TextOverflow.ellipsis,
+                                  color: Colors.grey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        chat.time,
-                        style: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                      //handle unread messages
-                      //if unread, show new message alert, else nothing
-                      chat.unread
-                          ? Container(
-                              width: 40.0,
-                              height: 20.0,
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.circular(30.0),
+                            const SizedBox(height: 5.0),
+                            Container(
+                              //media query allows you to handle & adapt to dynamic data
+                              //adjusts texts to 0.45 of width of sender row screen
+                              width: MediaQuery.of(context).size.width * 0.45,
+                              child: Text(
+                                chat.text,
+                                style: const TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                //hide long messages
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              alignment: Alignment.center,
-                              child: const Text(
-                                'NEW',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            )
-                          : const Text(''),
-                    ],
-                  ),
-                ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          chat.time,
+                          style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 5.0,
+                        ),
+                        //handle unread messages
+                        //if unread, show new message alert, else nothing
+                        chat.unread
+                            ? Container(
+                                width: 40.0,
+                                height: 20.0,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'NEW',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            : const Text(''),
+                      ],
+                    ),
+                  ],
+                ),
               );
             },
           ),
