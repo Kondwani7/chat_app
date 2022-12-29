681d61500bd117d8b70e3fcdb8460138b89d38ea
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-30 01:57:23 +0200

    starter file for the chat screen

---
+++ Desktop/flutter_apps/chat_app/lib/screens/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:chat_app/models/user_model.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  final User user;
+
+  ChatScreen({required this.user});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
