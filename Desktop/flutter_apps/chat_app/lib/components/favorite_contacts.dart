e3e5ac59bc634cfb01ac81d5f165855dce08bcf9
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-12-30 02:12:22 +0200

    click a favorite chat image to get to individual chat screen

--- Desktop/flutter_apps/chat_app/lib/components/favorite_contacts.dart
+++ Desktop/flutter_apps/chat_app/lib/components/favorite_contacts.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/models/message_model.dart';
+import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteContacts extends StatelessWidget {
@@ -42,24 +43,32 @@ class FavoriteContacts extends StatelessWidget {
               itemCount: favorites.length,
               itemBuilder: (BuildContext context, int index) {
                 //create avatars for our favorite contacts
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(children: <Widget>[
-                    CircleAvatar(
-                      radius: 35.0,
-                      //using our images from the images/ folder
-                      backgroundImage: AssetImage(favorites[index].imageUrl),
-                    ),
-                    const SizedBox(height: 6.0),
-                    Text(
-                      favorites[index].name,
-                      style: const TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
+                return GestureDetector(
+                  //tap favorite chat image to get to individual chat screen
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => ChatScreen(user: favorites[index])),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        //using our images from the images/ folder
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
                       ),
-                    )
-                  ]),
+                      const SizedBox(height: 6.0),
+                      Text(
+                        favorites[index].name,
+                        style: const TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ]),
+                  ),
                 );
               },
             ),
